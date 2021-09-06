cd009e3db1da70a55b86478cc3b6022b507513e8
Nikolay Nosenko <mnosenko@freeadmin.pp.ua>
2021-09-06 20:49:57 +0300

    Added first model, view, admin
    Signed-off-by: Nikolay Nosenko <mnosenko@freeadmin.pp.ua>

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Таблица "Товар"
+
+    Название товара
+    Ссылка
+    Цена товара
+    Дата и время создания записи
+    Двтв и время обновления записи
+    """
+    title = models.CharField(max_length=120)
+    link = models.URLField()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
