89fbc830a6b25131897897c29f74ec502da120d4
Nikolay Nosenko <mnosenko@freeadmin.pp.ua>
2021-09-08 14:23:52 +0300

    Rename project
    Signed-off-by: Nikolay Nosenko <mnosenko@freeadmin.pp.ua>

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,4 +1,4 @@
-"""workshop URL Configuration
+"""wishlist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
